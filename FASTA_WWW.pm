fa6d64bb37cd097a2d6edc4fc3b04580b235c3c8
William R Pearson <wrp@virginia.edu>
2026-08-30 20:43:26 -0400

    remove additional --host/TMP_DIR info

--- FASTA_WWW.pm
+++ FASTA_WWW.pm
@@ -1229,10 +1229,11 @@ sub display_result {
 ################
 # remove $TMP_DIR references from output
 #
-    $output =~ s%$TMP_DIR\/%%g;
+    $output =~ s%$TMP_DIR\/?%%g;
 #    $output =~ s%FA_WWW.*?\.%TMP.%g;
     $output =~ s%$BIN_DIR\/%%g;
     $output =~ s/\+\-\-host=[^+]+/+/g;
+    $output =~ s/ \-\-host=[^ ]+/ /g;
   }
 
   $output .= "\n" ;
