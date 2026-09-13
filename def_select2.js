34a4f8a462e14617ac5776f035e82802f7b3f33c
William R Pearson <wrp@virginia.edu>
2026-09-13 14:44:03 -0400

    correct missing quote in update_pgm()

--- def_select2.js
+++ def_select2.js
@@ -304,7 +304,7 @@ function update_pgm(form) {
 
     if (pgm_val == 'fap' || pgm_val == 'rss' ||
 	pgm_val == 'lal' || pgm_val == 'pal' || pgm_val == 'lpal' ||
-	pgm_val == 'sw' || pgm_val == 'psi2sw' || pgm_val == 'psi2gg || 
+	pgm_val == 'sw' || pgm_val == 'psi2sw' || pgm_val == 'psi2gg' || 
 	pgm_val == 'gnw' || pgm_val == 'lnw' ||
 	pgm_val == 'bp' ) {
       if (form['n_lib']) {form['n_lib'].disabled = true;}
